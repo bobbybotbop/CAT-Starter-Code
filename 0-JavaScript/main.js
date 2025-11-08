65e0a5af35769dc17cfafbb8a4ca6f4ff292c53b
bobbybotbop <bobbybotbop@gmail.com>
2025-11-08 14:48:03 -0500

    bruh

--- 0-JavaScript/main.js
+++ 0-JavaScript/main.js
@@ -2,22 +2,29 @@
 
 // Part 1: Types and Variables
 // Example: Make a reassignable variable, named bruh, that equals a string "bruh"
-let bruh = "bruh"
+let bruh = "bruh";
 // TODO 1: Make a reassignable variable, named five, that equals a number 5
+let five = 5;
 
 // TODO 2: Make a nonreassignable variable, named isTrue, that equals a boolean true
+const isTrue = true;
 
 // Part 2: Functions
 // Example: Write an empty function (no parameter, no output)
-const someFunction = () => {}
+const someFunction = () => {};
 function someOtherFunction() {}
 
 // TODO 3: Write your own empty function
+const func = () => {};
 
 // TODO 4: Finish this function, that takes an array of numbers and returns the sum of all the numbers
 const sumOfArray = (numbers) => {
-
-}
- 
+  let sum = 0;
+  numbers.forEach((num) => {
+    sum += num;
+  });
+  return sum;
+};
 
 // Very nice, les go into typescript after
+export {};
--- 1-TypeScript/main.ts
+++ 1-TypeScript/main.ts
@@ -2,32 +2,38 @@
 
 // Part 1: Types and Variables
 // Example: Make a reassignable variable, named bruh, that equals a string "bruh"
-let bruh: string = "bruh"
+let bruh: string = "bruh";
 // TODO 1: Make a reassignable variable, named five, that equals a number 5
-
+let five: number = 5;
 // TODO 2: Make a nonreassignable variable, named isTrue, that equals a boolean true
-
+const isTrue: boolean = true;
 
 // Part 2: Functions
 // Example: Write an empty function (no parameter, no output)
-const someFunction = () => {}
+const someFunction = () => {};
 function someOtherFunction() {}
 
 // TODO 3: Write your own empty function
+const func = () => {};
 
 // TODO 4: Finish this function, that takes an array of numbers and returns the sum of all the numbers
 const sumOfArray = (numbers: number[]) => {
- 
+  let ans = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    ans += numbers[i];
+  }
+  return ans;
 };
 
 // TODO 5: Finish this function, that takes an array of numbers and returns the sum of all the numbers
 const convertToCelsius = (fahrenheit: number) => {
-
+  return (fahrenheit - 32) * (5 / 9);
 };
 
 // TODO 6: Write your own function that adds two numbers. Also, annotate the return type
-
-
+const addTwo = (num1: number, num2: number): number => {
+  return num1 + num2;
+};
 
 // Part 3: Interfaces
 // Example: Write an interface for a car
@@ -42,8 +48,19 @@ interface Car {
 let myCar: Car = {
   brand: "Toyota",
   model: "Camry",
-  year: 2022
+  year: 2022,
 };
 
 // TODO 7: Write your own interface for a boat, with at least 3 properties. Also make an object that uses the interface
 
+interface boat {
+  squareFoot: number;
+  isSailBoat: boolean;
+  color: string;
+}
+
+let myBoat: boat = {
+  squareFoot: 67,
+  isSailBoat: true,
+  color: "green",
+};
